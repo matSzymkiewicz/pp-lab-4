5c39c4504d5c835407dde48674816dd78472d677
matSzymkiewicz <161924739+matSzymkiewicz@users.noreply.github.com>
2024-04-07 18:16:48 +0200

    Company.java
    Zmiana employeez indeksem 3 z worker na Employee

--- Company.java
+++ Company.java
@@ -8,7 +8,7 @@ public class Company {
         employees[0] = new Manager("Gal Anonim", 50000);
         employees[1] = new Worker("Mat Szy", 60000);
         employees[2] = new Employee("Mateusz Szymkiewicz", 55000);
-        employees[3] = new Worker("Brum Brum", 65000);
+        employees[3] = new Employee("Brum Brum", 65000);
         employees[4] = new Worker("Adam Mickiewicz", 70000);
         
         ((Manager) employees[0]).setNumberOfSubordinates(0);
